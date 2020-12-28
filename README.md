1b5dbcf32498788c054af6eac15cb0dc50234c34
Anthony Pena <anthony.pena@outlook.fr>
2020-12-28 12:10:05 +0100

    day 19 part 1 ok part 2 ko

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ Day | Part 1 | Part 2
 16 | :heavy_check_mark: <img src="https://www.rust-lang.org/static/images/rust-logo-blk.svg" width="20" /> | :heavy_check_mark: <img src="https://www.rust-lang.org/static/images/rust-logo-blk.svg" width="20" />
 17 | :heavy_check_mark: <img src="https://www.rust-lang.org/static/images/rust-logo-blk.svg" width="20" /> | :heavy_check_mark: <img src="https://www.rust-lang.org/static/images/rust-logo-blk.svg" width="20" />
 18 | :heavy_check_mark: <img src="https://www.rust-lang.org/static/images/rust-logo-blk.svg" width="20" /> | :heavy_check_mark: <img src="https://www.rust-lang.org/static/images/rust-logo-blk.svg" width="20" />
-19 | :clock6: | :clock6:
+19 | :heavy_check_mark: <img src="https://www.rust-lang.org/static/images/rust-logo-blk.svg" width="20" /> | :x:
 20 | :clock6: | :clock6:
 21 | :clock6: | :clock6:
 22 | :clock6: | :clock6:
--- day19/src/main.rs
+++ day19/src/main.rs
@@ -95,11 +95,11 @@ fn parse_data(input: String) -> Result<Data, String> {
 }
 
 fn part1(data: &Data) {
-    let rule_index = index_rules(data);
+    let rules_index = index_rules(data);
     let valid_message_count = data
         .messages
         .iter()
-        .map(|m| match_rule(&rule_index, 0, m))
+        .map(|m| match_rule(&rules_index, 0, m))
         .filter(|is_matching| *is_matching)
         .count();
     println!("Part1: {:?}", valid_message_count);
@@ -114,6 +114,8 @@ fn index_rules(data: &Data) -> HashMap<u32, &Rule> {
         };
         res.insert(id, r);
     }
+    // println!("index");
+    // res.iter().for_each(|r| println!("\t{:?}", r));
     return res;
 }
 
@@ -122,34 +124,108 @@ fn match_rule(rules_index: &HashMap<u32, &Rule>, rule: u32, msg: &String) -> boo
         rules_index_rec: &HashMap<u32, &Rule>,
         msg_rec: &String,
         rule_rec: u32,
-        msg_i: &mut usize,
-    ) -> bool {
+        msg_i: usize,
+        depth: usize,
+    ) -> (bool, usize) {
+        if msg_i >= msg_rec.len() {
+            println!("index out of bound {}/{}", msg_i, msg_rec);
+            return (false, msg_i);
+        }
+        let indent: String = (0..depth)
+            .into_iter()
+            .map(|_| -> &str { "  " })
+            .fold("".to_string(), |prev, c| format!("{}{}", prev, c));
+        println!(
+            "{}match_rule_rec(rule:{},msg_i:{})",
+            indent, rule_rec, msg_i
+        );
         return match rules_index_rec.get(&rule_rec) {
-            Some(Rule::Leaf(_, c)) => msg_rec.chars().nth(*msg_i).unwrap() == *c,
-            Some(Rule::Composite(_, patterns)) => patterns.iter().any(|sub_pattern| {
-                let mut msg_i2 = msg_i.clone();
-                return sub_pattern.iter().all(|rule_ref| {
-                    match_rule_rec(rules_index_rec, msg_rec, rule_ref.id, &mut msg_i2)
+            Some(Rule::Leaf(_, c)) => {
+                let res = msg_rec.chars().nth(msg_i).unwrap() == *c;
+                println!(
+                    "{}leaf {:?}({}) = {:?} => {}",
+                    indent,
+                    msg_rec.chars().nth(msg_i).unwrap(),
+                    msg_i,
+                    c,
+                    res
+                );
+                return (res, msg_i + 1);
+            }
+            Some(Rule::Composite(_, patterns)) => {
+                println!("{}any {:?}/{}", indent, patterns, msg_i);
+                let mut msg_i_res = msg_i;
+                let res_pattern = patterns.iter().any(|sub_pattern| {
+                    println!("{}all {:?}/{}", indent, sub_pattern, msg_i);
+                    let mut msg_i2 = msg_i;
+                    let res_sub_pattern = sub_pattern.iter().all(|rule_ref| {
+                        let (sub_pattern_res, msg_i3) = match_rule_rec(
+                            rules_index_rec,
+                            msg_rec,
+                            rule_ref.id,
+                            msg_i2,
+                            depth + 1,
+                        );
+                        if sub_pattern_res {
+                            msg_i2 = msg_i3;
+                        }
+                        return sub_pattern_res;
+                    });
+                    println!(
+                        "{}all {:?}/{} => {}",
+                        indent, sub_pattern, msg_i2, res_sub_pattern
+                    );
+                    if res_sub_pattern {
+                        msg_i_res = msg_i2;
+                    }
+                    res_sub_pattern
                 });
-            }),
+                println!("{}any {:?}/{} => {}", indent, patterns, msg_i, res_pattern);
+                (res_pattern, msg_i_res)
+            }
             _ => {
                 println!("Invalid rule index: {}", rule_rec);
-                return false;
+                return (false, msg_i);
             }
         };
     }
-    let mut msg_index: usize = 0;
-    return match_rule_rec(rules_index, msg, rule, &mut msg_index);
+    let (msg_valid, msg_index) = match_rule_rec(rules_index, msg, rule, 0, 0);
+    return msg_valid && msg_index >= msg.len();
 }
 
 fn part2(data: &Data) {
-    println!("Part2: {:?}", data.messages.len());
+    let mut rules_index = index_rules(data);
+    // 8: 42 | 42 8
+    let rule8 = Rule::Composite(
+        8,
+        vec![
+            vec![RuleRef::new(42)],
+            vec![RuleRef::new(42), RuleRef::new(8)],
+        ],
+    );
+    rules_index.insert(8, &rule8);
+    // 11: 42 31 | 42 11 31
+    let rule11 = Rule::Composite(
+        11,
+        vec![
+            vec![RuleRef::new(42), RuleRef::new(31)],
+            vec![RuleRef::new(42), RuleRef::new(11), RuleRef::new(31)],
+        ],
+    );
+    rules_index.insert(11, &rule11);
+    let valid_message_count = data
+        .messages
+        .iter()
+        .map(|m| match_rule(&rules_index, 0, m))
+        .filter(|is_matching| *is_matching)
+        .count();
+    println!("Part2: {:?}", valid_message_count);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    /*
     #[test]
     fn it_should_parse_correctly() {
         let input = "0: 4 1 5
@@ -158,7 +234,7 @@ mod tests {
         3: 4 5 | 5 4
         4: \"a\"
         5: \"b\"
-        
+
         ababbb
         bababa
         abbbab
@@ -213,7 +289,7 @@ mod tests {
         3: 4 5 | 5 4
         4: \"a\"
         5: \"b\"
-        
+
         ababbb
         bababa
         abbbab
@@ -257,6 +333,7 @@ mod tests {
 
         assert_eq!(index_rules(&rules), expected);
     }
+    */
 
     fn msg_should_match_rule_0(msg: &str) -> bool {
         let input = "0: 4 1 5
@@ -271,12 +348,10 @@ mod tests {
         let rules_index = index_rules(&rules);
         return match_rule(&rules_index, 0, &msg.to_string());
     }
-
     #[test]
     fn it_should_match_rule_1() {
         assert!(msg_should_match_rule_0("ababbb"));
     }
-
     #[test]
     fn it_should_match_rule_2() {
         assert!(msg_should_match_rule_0("abbbab"));
@@ -297,25 +372,116 @@ mod tests {
         assert!(!msg_should_match_rule_0("aaaabbb"));
     }
 
-    // #[test]
-    // fn it_should_match_rule_2() {
-    //     let input = "0: 4 1 5
-    //     1: 2 3 | 3 2
-    //     2: 4 4 | 5 5
-    //     3: 4 5 | 5 4
-    //     4: \"a\"
-    //     5: \"b\"
+    fn msg_should_match_rule_1(msg: &str) -> bool {
+        let input = "42: 9 14 | 10 1
+        9: 14 27 | 1 26
+        10: 23 14 | 28 1
+        1: \"a\"
+        5: 1 14 | 15 1
+        19: 14 1 | 14 14
+        12: 24 14 | 19 1
+        16: 15 1 | 14 14
+        31: 14 17 | 1 13
+        6: 14 14 | 1 14
+        2: 1 24 | 14 4
+        0: 8 11
+        13: 14 3 | 1 12
+        15: 1 | 14
+        17: 14 2 | 1 7
+        23: 25 1 | 22 14
+        28: 16 1
+        4: 1 1
+        20: 14 14 | 1 15
+        3: 5 14 | 16 1
+        27: 1 6 | 14 18
+        14: \"b\"
+        21: 14 1 | 1 14
+        25: 1 1 | 1 14
+        22: 14 14
+        26: 14 22 | 1 20
+        18: 15 15
+        7: 14 5 | 1 21
+        24: 14 1
+        8: 42 | 42 8
+        11: 42 31 | 42 11 31
+        
+        ababbb";
+        let rules = parse_data(input.to_string()).unwrap();
+        let rules_index = index_rules(&rules);
+        return match_rule(&rules_index, 0, &msg.to_string());
+    }
 
-    //     ababbb
-    //     bababa
-    //     abbbab
-    //     aaabbb
-    //     aaaabbb";
-    //     let rules = parse_data(input.to_string()).unwrap();
-    //     let rules_index = index_rules(&rules);
+    #[test]
+    fn it_should_match_rule_1_1() {
+        assert!(msg_should_match_rule_1("bbabbbbaabaabba"));
+    }
+    #[test]
+    fn it_should_match_rule_1_2() {
+        assert!(msg_should_match_rule_1("babbbbaabbbbbabbbbbbaabaaabaaa"));
+    }
+    /*
+    #[test]
+    fn it_should_match_rule_1_3() {
+        assert!(msg_should_match_rule_1(
+            "aaabbbbbbaaaabaababaabababbabaaabbababababaaa"
+        ));
+    }
+    #[test]
+    fn it_should_match_rule_1_4() {
+        assert!(msg_should_match_rule_1("bbbbbbbaaaabbbbaaabbabaaa"));
+    }
+    #[test]
+    fn it_should_match_rule_1_5() {
+        assert!(msg_should_match_rule_1(
+            "bbbababbbbaaaaaaaabbababaaababaabab"
+        ));
+    }
+    #[test]
+    fn it_should_match_rule_1_6() {
+        assert!(msg_should_match_rule_1("ababaaaaaabaaab"));
+    }
+    #[test]
+    fn it_should_match_rule_1_7() {
+        assert!(msg_should_match_rule_1("ababaaaaabbbaba"));
+    }
+    #[test]
+    fn it_should_match_rule_1_8() {
+        assert!(msg_should_match_rule_1("baabbaaaabbaaaababbaababb"));
+    }
+    #[test]
+    fn it_should_match_rule_1_9() {
+        assert!(msg_should_match_rule_1("abbbbabbbbaaaababbbbbbaaaababb"));
+    }
+    #[test]
+    fn it_should_match_rule_1_10() {
+        assert!(msg_should_match_rule_1("aaaaabbaabaaaaababaa"));
+    }
+    #[test]
+    fn it_should_match_rule_1_11() {
+        assert!(msg_should_match_rule_1(
+            "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa"
+        ));
+    }
+    #[test]
+    fn it_should_match_rule_1_12() {
+        assert!(msg_should_match_rule_1(
+            "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"
+        ));
+    }
 
-    //     assert!(!match_rule(&rules_index, 0, &"bababa".to_string()));
-    //     assert!(!match_rule(&rules_index, 0, &"aaabbb".to_string()));
-    //     assert!(!match_rule(&rules_index, 0, &"aaaabbb".to_string()));
-    // }
+    #[test]
+    fn it_should_match_rule_1_13() {
+        assert!(!msg_should_match_rule_1(
+            "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa"
+        ));
+    }
+    #[test]
+    fn it_should_match_rule_1_14() {
+        assert!(!msg_should_match_rule_1("aaaabbaaaabbaaa"));
+    }
+    #[test]
+    fn it_should_match_rule_1_15() {
+        assert!(!msg_should_match_rule_1("babaaabbbaaabaababbaabababaaab"));
+    }
+    */
 }
